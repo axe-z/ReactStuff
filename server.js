dc3d7195669b743c7ec87294cd6b01df6be5b044
info@axe-z.com <axez@MBP-AXE-Z.local>
2017-04-11 14:58:20 -0400

    changement de serveur

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+///creer app
+
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+///verifier que la personne a pas ete sur https, on sert sur http seulement.
+app.use(function (req, res, next){
+ if(req.headers['x-forwarded-proto'] === 'http'){
+   next()    //next est comme return ... tout est beau
+ } else {
+   res.redirect('http://' + req.hostname + req.url )  //si sur https, remet sur http
+ }
+})
+
+
+app.use(express.static('build'));
+
+app.listen(PORT, function (){
+ console.log('ca roule... sur le port: ' + PORT )
+});
